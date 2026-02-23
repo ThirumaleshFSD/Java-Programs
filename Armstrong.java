8871df714c6490f63d30058101cdd09c31506220
thirumalesh-validprofile <thirumalesh@validprofile.com>
2026-02-23 16:27:58 +0530

    java programs

---
+++ Armstrong.java
@@ -0,0 +1,17 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 372;
+        int originalNum = num;
+        int result = 0;
+        while (originalNum != 0) {
+            int remainder = originalNum % 10;
+            result += Math.pow(remainder, 3);
+            originalNum /= 10;
+        }
+        if (result == num) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+    }
+}
