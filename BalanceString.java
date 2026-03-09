013d59fe9cdee8c9a53c65e8ef9156fc0c8c0f1a
ThirumaleshFSD <sirisampadha939184@gmail.com>
2026-03-09 10:22:34 +0530

    Code on String balance

--- BalanceString.java
+++ BalanceString.java
@@ -1,3 +1,33 @@
+
+import java.util.Stack;
+
 public class BalanceString {
-    
-}
+    public static void main(String[] args) {
+          String s="{([])}";
+        System.out.println(isBal(s));
+    }
+    public static boolean isBal(String str){
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch= str.charAt(i);
+            System.out.println(ch);
+            
+            if(ch=='{'||ch=='('||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty())
+                    return false;
+
+                char top=st.pop();
+                if((ch=='}'&&top!='{')||(ch==')'&&top!='(')||(ch==']'&&top!='[')){
+
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
+    }
+
