d92dd59b81f2f446f7fabb1f62c4f695e2721394
Thiru Sampath <sirisampadha939184@gmail.com>
2026-03-30 21:03:52 +0530

    Clarify sum variable type with a comment
    Added a comment to clarify that sum is in double.

--- Average.java
+++ Average.java
@@ -2,6 +2,7 @@ public class Average {
     public static void main(String[] args) {
         int a[]={1,2,3,4,5};
         double sum=0;
+        //sum in double
         for(int i=0;i<a.length;i++){
             sum+=a[i];
         }
