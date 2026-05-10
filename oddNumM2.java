4e4da54dcbc90c1d186a6878471a5655ce73640e
Thiru Sampath <sirisampadha939184@gmail.com>
2026-05-10 14:55:03 +0530

    Add comment for clarity on odd number printing
    Added a comment to explain the purpose of the loop.

--- oddNumM2.java
+++ oddNumM2.java
@@ -1,6 +1,7 @@
 public class oddNumM2 {
     public static void main(String[] args) {
        int n=49;
+        //print the odd numbers by using for loop from 1 to 50
        for(int i=n;i>=1;i=i-2){
         System.out.println(i);
        }
